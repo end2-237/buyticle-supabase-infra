b32b8f65f6c721a154ccd52e6e32d57929cb7706
end2-237 <dnsoga0@gmail.com>
2026-04-30 18:12:18 +0000

    v2.4.1

--- init.sql
+++ init.sql
@@ -3,10 +3,10 @@ DO $$ BEGIN
     CREATE ROLE supabase_admin WITH LOGIN PASSWORD 'buyticle' SUPERUSER BYPASSRLS;
   END IF;
   IF NOT EXISTS (SELECT FROM pg_roles WHERE rolname = 'anon') THEN
-    CREATE ROLE anon NOLOGIN;
+    CREATE ROLE anon NOLOGIN BYPASSRLS;
   END IF;
   IF NOT EXISTS (SELECT FROM pg_roles WHERE rolname = 'authenticated') THEN
-    CREATE ROLE authenticated NOLOGIN;
+    CREATE ROLE authenticated NOLOGIN BYPASSRLS;
   END IF;
   IF NOT EXISTS (SELECT FROM pg_roles WHERE rolname = 'service_role') THEN
     CREATE ROLE service_role NOLOGIN BYPASSRLS;
@@ -14,7 +14,7 @@ DO $$ BEGIN
 END $$;
 
 GRANT ALL ON SCHEMA public TO anon, authenticated, service_role;
-GRANT ALL ON SCHEMA storage TO service_role, supabase_admin;
-GRANT ALL ON ALL TABLES IN SCHEMA storage TO service_role, supabase_admin;
-GRANT ALL ON ALL SEQUENCES IN SCHEMA storage TO service_role, supabase_admin;
+GRANT USAGE ON SCHEMA storage TO anon, authenticated, service_role, supabase_admin;
+GRANT ALL ON ALL TABLES IN SCHEMA storage TO anon, authenticated, service_role, supabase_admin;
+GRANT ALL ON ALL SEQUENCES IN SCHEMA storage TO anon, authenticated, service_role, supabase_admin;
 GRANT ALL ON DATABASE postgres TO supabase_admin;
